8a9dadc560d8cb5f207a8ad411d7ae5b3bf5dca9
Daniel Zadák <44667706+silverhound4d@users.noreply.github.com>
2023-01-30 11:22:59 +0100

    Update file_io.py

--- file_io.py
+++ file_io.py
@@ -14,11 +14,11 @@ class FileIO:
     def copy_file(source: Path, target: Path) -> None:
         """Copy a file and verify its integrity."""
         with open(source, "rb") as source_file:
-            log.logger.info(f"Read file from: {source}")
+            log.logger.debug(f"Read file from: {source}")
             with open(target, "wb+") as target_file:
                 for part in source_file:
                     target_file.write(part)
-                log.logger.info(f"Copied file to: {target}")
+                log.logger.debug(f"Copied file to: {target}")
                 Utils.compare_hashed_files(Utils.hash_file(source_file), Utils.hash_file(target_file))
 
     @staticmethod
@@ -31,12 +31,12 @@ class FileIO:
             # Create directories.
             if path_object.is_dir() and not backup_abs_path.exists():
                 os.mkdir(backup_abs_path)
-                log.logger.debug(f"Directory created: {backup_abs_path}")
+                log.logger.info(f"Directory created: {backup_abs_path}")
 
             # Create files.
             if path_object.is_file() and not backup_abs_path.exists():
                 FileIO.copy_file(path_object, backup_abs_path)
-                log.logger.debug(f"File successfully copied to: {backup_abs_path}")
+                log.logger.info(f"File successfully copied to: {backup_abs_path}")
 
     @staticmethod
     def delete_file_system_objects(backup_folder_path: Path, source_folder_path: Path) -> None:
@@ -51,16 +51,16 @@ class FileIO:
             if path_object.is_file():
                 if not source_abs_path.exists():
                     os.remove(backup_abs_path)
-                    log.logger.debug(f"File removed: {backup_abs_path}")
+                    log.logger.info(f"File removed: {backup_abs_path}")
 
             # Remove directories
             if path_object.is_dir():
                 if not source_abs_path.exists():
                     os.rmdir(backup_abs_path)
-                    log.logger.debug(f"Directory removed: {backup_abs_path}")
+                    log.logger.info(f"Directory removed: {backup_abs_path}")
 
     def sync_folders(self) -> None:
         """Synchronize select folders."""
-        log.logger.info("==== Initializing folder synchronization ====")
+        log.logger.debug("==== Initializing folder synchronization ====")
         self.create_file_system_objects(self.backup_folder, self.source_folder)
         self.delete_file_system_objects(self.backup_folder, self.source_folder)
