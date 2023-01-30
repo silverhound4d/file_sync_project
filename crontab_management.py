5af721348806e8b7a14e152fd3886809db5945a9
Daniel Zadák <44667706+silverhound4d@users.noreply.github.com>
2023-01-30 11:21:59 +0100

    Update crontab_management.py

--- crontab_management.py
+++ crontab_management.py
@@ -5,7 +5,7 @@ import logger as log
 def schedule_file_sync_cron_job(parsed_args) -> None:
     """Setup a cron daemon to repeatedly sync select folders."""
     if parsed_args.interval is not None:
-        log.logger.debug("Initializing file_sync cron job")
+        log.logger.info("Initializing file_sync cron job")
         job = (
             f"""*/{parsed_args.interval} * * * * python3 main.py """
             f"""{parsed_args.backup} {parsed_args.source} {parsed_args.log_path}"""
@@ -13,4 +13,4 @@ def schedule_file_sync_cron_job(parsed_args) -> None:
         mem_cron = CronTab(tab=job)
 
         for _ in mem_cron.run_scheduler():  # Cron daemon.
-            log.logger.info(f"Cron: RUNNING {job}")
+            log.logger.debug(f"Cron: RUNNING {job}")
